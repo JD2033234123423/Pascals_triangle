f486b5c8d66fd6c1da435fe048c1a8002d7fee29
JD2033234123423 <119131374+JD2033234123423@users.noreply.github.com>
2023-04-16 11:24:07 +0100

    Update and rename question3.py to Pascals_triangle.py

--- Pascals_triangle.py
+++ Pascals_triangle.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 
-#################################################################################################################
-#                                                                                                               #                                                                                                               
-#                                          READ ME:                                                             #
-#                                                                                                               #
-#                 To use the program please have python 3 installed.                                            #
-#                 Then run the program using python3 as the interpreter.                                        #
-#                 Simply run:                                                                                   #
-#                 python3 question3.py                                                                          #
-#                                                                                                               #
-#                                                                                                               #
-#                 WARNING:                                                                                      #
-#                                                                                                               #
-#                 This module only accepts non-negative integers e.g., 0,1,2,                                   #
-#                 Any other value will generate an error message and take no action                             #
-#                                                                                                               #
-#################################################################################################################
-
 # Define the function factorial and what is passed in 
 def factorial(n):
     # Implement a check to see if n is a non-negative integer
